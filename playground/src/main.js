9e5fcb70ab6d1091ee435c22867f5085ffe13983
Denis Danilov <d.danilov@duotek.ru>
2024-07-07 18:11:11 +0300

    Temp

--- playground/src/main.js
+++ playground/src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
+import { createRouter, createMemoryHistory } from 'vue-router'
+
 import App from './App.vue'
+
+import Index from '../pages/Index.vue'
+import Modal from '../pages/Modal.vue'
 
 import '@mrzlanx532/nuxt-3-custom-scroll-plugin/dist/css/common.css';
 import '@mrzlanx532/nuxt-3-custom-scroll-plugin/dist/css/document-scroll.no-ssr.css';
@@ -7,8 +12,19 @@ import '@mrzlanx532/nuxt-3-custom-scroll-plugin/dist/css/v-scrollable.css';
 
 import { Scrollable } from '@mrzlanx532/nuxt-3-custom-scroll-plugin'
 
+const routes = [
+    { path: '/', component: Index },
+    { path: '/modal', component: Modal }
+]
+
+const router = createRouter({
+    history: createMemoryHistory(),
+    routes
+})
+
 const app = createApp(App)
 
+app.use(router)
 app.directive('scrollable', {
     mounted: function (el, binding) {
         new Scrollable(el, binding.value)
